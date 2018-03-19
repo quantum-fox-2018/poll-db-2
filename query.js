93a8a4b6455ce30a372e293f86394abec1b16202
barestu <fheartnet@gmail.com>
2018-03-19 20:45:44 +0700

    Minor Fix

--- query.js
+++ query.js
@@ -8,25 +8,27 @@ GROUP BY  Politicians.id
 ORDER BY Politicians.grade_current ASC;
 
 //2. 3 Politicians highest votes & voters
-SELECT (SELECT COUNT(*)
-				FROM Votes
-				WHERE politicianId = Politicians.id
-				GROUP BY politicianId
-			) AS totalVote, Politicians.name AS politicianName, (Voters.first_name||' ' ||Voters.last_name) AS voterName, Voters.gender
+SELECT
+(SELECT COUNT(*)
+	FROM Votes
+	WHERE politicianId = Politicians.id
+	GROUP BY politicianId
+) AS totalVote, Politicians.name AS politicianName, (Voters.first_name||' ' ||Voters.last_name) AS voterName, Voters.gender
 FROM Politicians
 JOIN Votes
 	ON Politicians.id = Votes.politicianId
 JOIN Voters
 	ON Voters.id = Votes.voterId
 WHERE Politicians.id
-IN (SELECT Politicians.id
-		FROM Votes
-		JOIN Politicians
-		ON Votes.politicianId = Politicians.id
-		GROUP BY Politicians.id
-		ORDER BY COUNT(Votes.id) DESC
-		LIMIT 3
-	)
+IN
+(SELECT Politicians.id
+  FROM Votes
+  JOIN Politicians
+  ON Votes.politicianId = Politicians.id
+  GROUP BY Politicians.id
+  ORDER BY COUNT(Votes.id) DESC
+  LIMIT 3
+)
 ORDER BY politicianName DESC;
 
 //3. List curang, cara HAVING
@@ -39,11 +41,12 @@ HAVING totalVote > 1
 ORDER BY totalVote DESC
 
 //3. List curang, cara Subquery
-SELECT (SELECT COUNT(*)
-				FROM Votes
-				WHERE voterId = Voters.id
-				GROUP BY voterId
-			) AS totalVotes,
+SELECT
+(SELECT COUNT(*)
+	FROM Votes
+	WHERE voterId = Voters.id
+	GROUP BY voterId
+) AS totalVotes,
 (Voters.first_name||' '||Voters.last_name) AS name, Voters.gender, Voters.age
 FROM Voters
 JOIN Votes
@@ -51,4 +54,3 @@ JOIN Votes
 WHERE totalVotes > 1
 GROUP BY Voters.id
 ORDER BY totalVotes DESC;
-	
